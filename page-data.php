781a002755d2f8d4825b6ebee8a3856b12f05339
Simi <simi.damani@austintexas.gov>
2018-07-20 19:01:26 -0500

    refactoring to handle multiple datasets.

--- page-data.php
+++ page-data.php
@@ -44,36 +44,69 @@ get_header();
   // See js/tji-datasets-explore.js
   jQuery(function(){
     var chartView = new TJIChartView({
-      compressed_data_json_url: '/cdr_compressed.json',
-      complete_data_csv_url: '/cdr_full.csv',
-      chart_configs: [
-        {type: 'bar', group_by: 'year'},
-        {type: 'doughnut', group_by: 'race'},
-        {type: 'doughnut', group_by: 'sex'},
-        {type: 'doughnut', group_by: 'manner_of_death'},
-        {type: 'doughnut', group_by: 'age_group', sort_by: {column: 'key', direction: 'asc'}},
-        {type: 'doughnut', group_by: 'type_of_custody'},
-        {type: 'doughnut', group_by: 'death_location_type'},
-        {type: 'doughnut', group_by: 'means_of_death'},
-      ],
-      filter_configs: [
-        {'name': 'year'},
-        {'name': 'race'},
-        {'name': 'sex'},
-        {'name': 'manner_of_death'},
-        {'name': 'age_group'},
-        {'name': 'type_of_custody'},
-        {'name': 'death_location_type'},
-        {'name': 'means_of_death'},
-        {'name': 'agency_county', 'type': 'autocomplete'},
-        {'name': 'death_location_county', 'type': 'autocomplete'},
-      ],
+      datasets: [{
+        urls: {
+          compressed: '/cdr_compressed.json',
+          full: '/cdr_full.csv',
+        },
+        chart_configs: [
+          {type: 'bar', group_by: 'year'},
+          {type: 'doughnut', group_by: 'race'},
+          {type: 'doughnut', group_by: 'sex'},
+          {type: 'doughnut', group_by: 'manner_of_death'},
+          {type: 'doughnut', group_by: 'age_group', sort_by: {column: 'key', direction: 'asc'}},
+          {type: 'doughnut', group_by: 'type_of_custody'},
+          {type: 'doughnut', group_by: 'death_location_type'},
+          {type: 'doughnut', group_by: 'means_of_death'},
+        ],
+        filter_configs: [
+          {'name': 'year'},
+          {'name': 'race'},
+          {'name': 'sex'},
+          {'name': 'manner_of_death'},
+          {'name': 'age_group'},
+          {'name': 'type_of_custody'},
+          {'name': 'death_location_type'},
+          {'name': 'means_of_death'},
+          {'name': 'agency_county', 'type': 'autocomplete'},
+          {'name': 'death_location_county', 'type': 'autocomplete'},
+        ],
+      }],
       charts_elt_selector: '#js-TJIChartView',  
       filters_elt_selector: '#js-TJIChartViewFilters',  
       chart_wrapper_template: '<div class="col-sm-12 col-lg-6" />',  
       chartview_summary_template: '<div class="col-sm-12" />',
     });
   })
+</script>
+<script>
+  // // See js/tji-datasets-explore.js
+  // jQuery(function(){
+  //   var chartView = new TJIChartView({
+  //     compressed_data_json_url: '/ois_compressed.json',
+  //     complete_data_csv_url: '/ois_full.csv',
+  //     chart_configs: [
+  //       {type: 'bar', group_by: 'year'},
+  //       {type: 'doughnut', group_by: 'civilian_race'},
+  //       {type: 'doughnut', group_by: 'civilian_gender'},
+  //       {type: 'doughnut', group_by: 'civilian_died'},
+  //       {type: 'doughnut', group_by: 'deadly_weapon'},
+  //     ],
+  //     filter_configs: [
+  //       {'name': 'year'},
+  //       {'name': 'civilian_race'},
+  //       {'name': 'civilian_gender'},
+  //       {'name': 'civilian_died'},
+  //       {'name': 'deadly_weapon'},
+  //       {'name': 'agency_county', 'type': 'autocomplete'},
+  //       {'name': 'incident_county', 'type': 'autocomplete'},
+  //     ],
+  //     charts_elt_selector: '#js-TJIChartView2',  
+  //     filters_elt_selector: '#js-TJIChartViewFilters2',  
+  //     chart_wrapper_template: '<div class="col-sm-12 col-lg-6" />',  
+  //     chartview_summary_template: '<div class="col-sm-12" />',
+  //   });
+  // })
 </script>
 
 <?php
