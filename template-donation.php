868148e208191994796eae974379dd3a2b6a9f7b
Jason Zinn <jay.zinn@gmail.com>
2018-10-28 13:37:01 -0500

    Included newer version of jQuery on the donate page required for PayPal functionality

--- template-donation.php
+++ template-donation.php
@@ -179,6 +179,9 @@ get_sidebar('about');
 
 ?>
 
+<!-- Including a newer version of jQuery here needed for PayPal functionality -->
+<!-- We can remove the redundant jQuery include once we move away from WordPress -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/tji-donate.js"></script>
 
